d409230c51b1018d8726151894ed0c3f82adf577
RForsti1 <ryan.forstie1@t-mobile.com>
2020-04-17 14:01:19 -0700

    Add config properties

--- src/main/java/io/javabrains/springbootconfig/GreetingController.java
+++ src/main/java/io/javabrains/springbootconfig/GreetingController.java
@@ -1,5 +1,6 @@
 package io.javabrains.springbootconfig;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,8 +24,11 @@ public class GreetingController {
     @Value("#{${dbValues}}")
     private Map<String, String> dbValues;
 
+    @Autowired
+    private DbSettings dbSettings;
+
     @GetMapping("/greeting")
     public String greeting() {
-        return greetingMessage + " " + listValues + " " + staticMessage + " " + dbValues;
+        return greetingMessage + " " + listValues + " " + staticMessage + " " + dbValues + " " + dbSettings.getConnection() + dbSettings.getHost();
     }
 }
